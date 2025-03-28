f912bcf7504042814abe13d0177eb0fad2a701a6
Jack <jack.vlink@gmail.com>
2025-03-28 16:17:11 +0800

    jekyll test

--- content/about.md
+++
@@ -1,5 +0,0 @@
-+++
-title = 'About'
-date = 2023-11-04T17:09:33+08:00
-draft = true
-+++
--- themes/Anatolo/scripts/index.js
+++
@@ -1,6 +0,0 @@
-require('../includes/tasks/welcome');
-require('../includes/generators/insight')(hexo);
-require('../includes/generators/site_json')(hexo);
-require('../includes/generators/tags')(hexo);
-require('../includes/helpers/site')(hexo);
-require('../includes/helpers/tag')(hexo);
